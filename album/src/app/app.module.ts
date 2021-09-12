690b58abc5e66b90a140524ee4d900b7fa6398f4
shimadasoftware <73977456+shimadasoftware@users.noreply.github.com>
2021-09-11 21:52:40 -0500

    Add files via upload

---
+++ album/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ContentComponent } from './components/content/content.component';
+import { FormsModule } from '@angular/forms';
+
+import { ToastrModule} from 'ngx-toastr';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ContentComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ToastrModule.forRoot(),
+    ModalModule.forRoot(),
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
